3e75f7a88954b80a347aa74092bffdc49443b0fd
san-e <itzepyc@proton.me>
2023-12-02 16:26:43 +0100

    day-02: complete

---
+++ day-02/main.py
@@ -0,0 +1,65 @@
+import sys
+sys.path.append("..")
+from aoc_common import get_input, get_test_input
+import math
+from collections import defaultdict
+
+def parse(input: str):
+    out = {}
+    for game in input:
+        game_info = game.split(":")
+        game_id = int(game_info[0].replace("Game ", ""))
+        sets = game_info[1].strip().split(";")
+        throws = [x.strip().split(", ") for x in sets]
+        out[game_id] =  [[y.split(" ") for y in x] for x in throws]
+        
+    return out
+
+def main_p1(input: str, colors: dict):
+    games = parse(input)
+    possible_ids = set()
+
+    for id, game in games.items():
+        possible = True
+        for throw in game:
+            for amount, color in throw:
+                if int(amount) > colors[color]:
+                    possible = False
+        
+        if possible:
+            possible_ids.add(id)
+
+    return sum(possible_ids)
+
+def main_p2(input: str):
+    games = parse(input)
+    products = []
+    for id, game in games.items():
+        mongoose = defaultdict(list)
+        for throw in game:
+            for amount, color in throw:
+                mongoose[color].append(int(amount))
+        
+        products.append(math.prod([max([x for x in amount]) for amount in mongoose.values()]))
+
+    return sum(products)
+
+
+
+
+if __name__ == "__main__":
+    limits = {"red": 12, "green": 13, "blue": 14}
+
+    test_input, test_answer = get_test_input("./test-input-p1.txt")
+    assert main_p1(test_input, limits) == int(test_answer)
+    print("Test 1 passed")
+
+    test_input, test_answer = get_test_input("./test-input-p2.txt")
+    assert main_p2(test_input) == int(test_answer)
+    print("Test 2 passed")
+
+    input = get_input("input.txt")
+    answer1 = main_p1(input, limits)
+    answer2 = main_p2(input)
+
+    print(f"Part 1: {answer1}\nPart 2: {answer2}")
